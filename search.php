9739905f95e9d4f75fe2231387b46578dc329067
toshiaki miyagoshi <contact@54web.org>
2023-08-11 13:27:14 +0900

    検索一覧　ループ　テンプレートタグ化

--- search.php
+++ search.php
@@ -6,116 +6,39 @@
 <section class="inner-hero parallax-container   one-col " style="">
   <div class="wrapper text-center">
     <p class="m-b-10 h5">Search Results for</p>
-
-    <h1 class="text-white  ">aaa</h1>
-  
+    <h1 class="text-white  "><?php the_search_query(); ?></h1>
   </div>
 </section>
 
 <section class="search-content">
 
   <!---------------------- loop ---------------------->
+  <?php if (have_posts()): ?>
+  <?php while(have_posts()) : the_post();?>
   <div class="wrapper search-content--wrapper will-animate" data-animation="fall-up">
-    <a class="search-content--post flex flex-wrap align-start m-b-30 p-t-30" href="https://www.topstep.com/blog/choosing-the-right-key-trading-levels-part-2/">
+    <a class="search-content--post flex flex-wrap align-start m-b-30 p-t-30" href="<?php the_permalink(); ?>">
       <picture class="attachment-thumbnail--container size-thumbnail--container">
-        <img width="150" height="150" data-src="https://www.topstep.com/wp-content/uploads/2020/12/TopstepFeature-150x150.png" class="lazy attachment-thumbnail size-thumbnail" alt="" decoding="async" loading="lazy" />
+        <?php if ( has_post_thumbnail() ): ?>
+        <img src="<?php the_post_thumbnail_url(); ?>">
+        <?php else: ?>
+        <img width="150" height="150" data-src="<?php echo esc_url(home_url()) ; ?>/wp-content/uploads/2023/08/TopstepFeature-150x150.png" class="lazy attachment-thumbnail size-thumbnail" alt="" decoding="async" loading="lazy" />
+        <?php endif; ?>
       </picture>
       <div class="search-content--content editor-content">
-        <p><strong>Choosing The Right Key Trading Levels – Part 2</strong></p>
+        <p><strong><?php the_title(); ?></strong></p>
         <div class="search-content--copy flex space-between align-start">
           <p>
-          PETER DAVIES
-          January 14, 2020 01:00 AM
-          Predatory traders watch key levels
-          Traders commonly looking for either a trade...</p>
+          <?php the_author(); ?>
+          <?php echo get_post_time('F j, Y'); ?>
+          <?php the_time(); ?>
+          <?php echo mb_substr(strip_tags($post-> post_content),0,80).'...'; ?></p>
           <span class="button">View</span>
         </div>
       </div>
     </a>
   </div>
-  <!---------------------- // loop ---------------------->
-
-  <!---------------------- loop ---------------------->
-  <div class="wrapper search-content--wrapper will-animate" data-animation="fall-up">
-    <a class="search-content--post flex flex-wrap align-start m-b-30 p-t-30" href="https://www.topstep.com/blog/rules-based-trading-using-frameworks/">
-      <picture class="attachment-thumbnail--container size-thumbnail--container">
-        <img width="150" height="150" data-src="https://www.topstep.com/wp-content/uploads/2020/12/TopstepFeature-150x150.png" class="lazy attachment-thumbnail size-thumbnail" alt="" decoding="async" loading="lazy" />
-      </picture>
-      <div class="search-content--content editor-content">
-        <p><strong>Rules-Based Trading Using Frameworks</strong></p>
-        <div class="search-content--copy flex space-between align-start">
-          <p>
-          SEAN KOZAK
-          August 4, 2020 03:47 PM
-          NSTA Trading Framework
-          Too much information can be too much of a...
-          </p>
-          <span class="button">View</span>
-        </div>
-      </div>
-    </a>
-  </div>
-  <!---------------------- // loop ---------------------->
-
-  <!---------------------- loop ---------------------->
-  <div class="wrapper search-content--wrapper will-animate" data-animation="fall-up">
-    <a class="search-content--post flex flex-wrap align-start m-b-30 p-t-30" href="https://www.topstep.com/blog/day-trading-futures-without-charts-using-volume-profile/">
-      <picture class="attachment-thumbnail--container size-thumbnail--container">
-        <img width="150" height="150" data-src="https://www.topstep.com/wp-content/uploads/2020/12/TopstepFeature-150x150.png" class="lazy attachment-thumbnail size-thumbnail" alt="" decoding="async" loading="lazy" />
-      </picture>
-      <div class="search-content--content editor-content">
-        <p><strong>Day Trading Futures Without Charts Using Volume Profile</strong></p>
-        <div class="search-content--copy flex space-between align-start">
-          <p>
-          PETER DAVIES
-          November 18, 2019 12:30 PM
-          Volume Profile displays total futures contract volume traded at each price
-          Traders...
-          </p>
-          <span class="button">View</span>
-        </div>
-      </div>
-    </a>
-  </div>
-  <!---------------------- // loop ---------------------->
-
-  <!---------------------- loop ---------------------->
-  <div class="wrapper search-content--wrapper will-animate" data-animation="fall-up">
-    <a class="search-content--post flex flex-wrap align-start m-b-30 p-t-30" href="https://www.topstep.com/blog/understanding-why-futures-and-commodity-prices-move/">
-      <picture class="attachment-thumbnail--container size-thumbnail--container">
-        <img width="150" height="150" data-src="https://www.topstep.com/wp-content/uploads/2020/12/TopstepFeature-150x150.png" class="lazy attachment-thumbnail size-thumbnail" alt="" decoding="async" loading="lazy" />
-      </picture>
-      <div class="search-content--content editor-content">
-        <p><strong>Understanding Why Futures and Commodity Prices Move</strong></p>
-        <div class="search-content--copy flex space-between align-start">
-          <p>
-          PETER DAVIES
-          February 5, 2020 12:30 PM
-          Traders need to learn the fundamentals of why markets move
-          Why markets...
-          </p>
-          <span class="button">View</span>
-        </div>
-      </div>
-    </a>
-  </div>
-  <!---------------------- // loop ---------------------->
-
-  <!---------------------- loop ---------------------->
-  <div class="wrapper search-content--wrapper will-animate" data-animation="fall-up">
-    <a class="search-content--post flex flex-wrap align-start m-b-30 p-t-30" href="https://www.topstep.com/video/can-your-perfect-trade-setup-fail/">
-      <picture class="attachment-thumbnail--container size-thumbnail--container">
-        <img width="150" height="150" data-src="https://www.topstep.com/wp-content/uploads/2021/05/Can-Your-Perfect-Setup-Fail-150x150.png" class="lazy attachment-thumbnail size-thumbnail" alt="Can Your Perfect Setup Fail" decoding="async" loading="lazy" />
-      </picture>
-      <div class="search-content--content editor-content">
-        <p><strong>Can Your Perfect Trade Setup Fail?</strong></p>
-        <div class="search-content--copy flex space-between align-start">
-          <p>Can your perfect trade setup fail? The easiest way to answer this question is by first...</p>
-          <span class="button">View</span>
-        </div>
-      </div>
-    </a>
-  </div>
+  <?php endwhile; ?>
+  <?php endif; ?>
   <!---------------------- // loop ---------------------->
 
 </section>   
